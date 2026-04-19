f10beb40b6b44058c69715b16f215254bebacf31
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-19 09:53:28 +0500

    Created order and create user and search item added in header

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Order = () => {
-  return <div>Order</div>;
+  const order = useLoaderData();
+  console.log("Order data", order);
+  const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
+    order;
+
+  return (
+    <div>
+      <div>
+        <h2>Status</h2>
+        <div>
+          {priority && <span>Priority</span>}
+          <span>{status} order</span>
+        </div>
+      </div>
+
+      <div>
+        <p>{estimatedDelivery}</p>
+        <p>(Estimated Deliver: {estimatedDelivery})</p>
+      </div>
+
+      <div>
+        <p>Price Pizza: {orderPrice}</p>
+        {priority && <p>Price priority: {priorityPrice}</p>}
+        <p>To pay on delivery: {orderPrice + priorityPrice}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
 export async function getMenu() {
@@ -18,7 +20,44 @@ export async function getOrder(id) {
   return data;
 }
 
+export async function createOrder(newOrder) {
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: "POST",
+      body: JSON.stringify(newOrder),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res) throw Error();
+    const { data } = await res.json();
+    return data;
+  } catch {
+    throw Error("Failed creating your order");
+  }
+}
+
 export async function loader() {
   const menu = await getMenu();
   return menu;
+}
+
+export async function orderLoader({ params }) {
+  const order = await getOrder(params.orderId);
+  return order;
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
 }

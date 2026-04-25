b37e7782510517b819d0a44076cc2e9ad31600c5
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-25 17:40:42 +0500

    Styled component using TailwindCSS

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+import LinkButton from "./LinkButton";
 
 const Error = () => {
-  const navigate = useNavigate();
   const error = useRouteError();
-  console.log(error);
 
   return (
     <div className="error">
       <h1>Something went wrong!</h1>
       <p>{error.data}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <LinkButton to={-1}>&larr; Go back</LinkButton>
     </div>
   );
 };
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,9 +4,12 @@ import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between bg-yellow-400 uppercase sm:px-6 border-b border-stone-200 px-4 py-3">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <p className="hidden text-sm font-semibold md:block">Jonas</p>
     </header>
   );
 };

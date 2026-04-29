5edf24415e3fd30d5e6106f7bb253e36b1f91226
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-29 15:34:23 +0500

    Customized Button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ children, disabled, to }) => {
+const Button = ({ children, disabled, to, type }) => {
   const className =
     "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed cursor-pointer sm:px-6 sm:py-4";
 
+  const styles = {
+    primary: className + "px-4 py-3 sm:px-6 sm:py-4",
+    small: className + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed cursor-pointer px-4 py-2.5 md:px-6 md:py-3.5",
+  };
+
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );

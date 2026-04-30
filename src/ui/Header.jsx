0d22c87d77ef0adc64947e5679dc74f029d21b78
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-30 18:25:51 +0500

    Displayed username

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const username = useSelector((state) => state.user.username);
   return (
     <header className="flex items-center justify-between bg-yellow-400 uppercase sm:px-6 border-b border-stone-200 px-4 py-3">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
       <SearchOrder />
-      <p className="hidden text-sm font-semibold md:block">Jonas</p>
+      <p className="hidden text-sm font-semibold md:block">
+        {username !== "" ? username : null}
+      </p>
     </header>
   );
 };

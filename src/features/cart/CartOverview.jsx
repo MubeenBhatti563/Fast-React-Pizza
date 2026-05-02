0b5b19d9090eb2e3aafadff35e94e1366424672a
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-05-02 14:17:41 +0500

    Show when item is added

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 const CartOverview = () => {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
   return (
     <div className="flex items-center justify-between bg-stone-800 text-stone-200 uppercase p-4 text-sm sm:px-6 md:text-base">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>${totalCartPrice}</span>
       </p>
       <Link to="/cart">Open cart</Link>
     </div>

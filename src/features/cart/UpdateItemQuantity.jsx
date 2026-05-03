0589602dbda122d96c60f1957bbf9f926d4881c4
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-05-03 18:21:35 +0500

    Increment or Decrement cart items

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { decreaseItem, increaseItem } from "./cartSlice";
+
+const UpdateItemQuantity = ({ pizzaId, currentIncrement }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-1 md:gap-3">
+      <Button type="round" onClick={() => dispatch(decreaseItem(pizzaId))}>
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentIncrement}</span>
+      <Button type="round" onClick={() => dispatch(increaseItem(pizzaId))}>
+        +
+      </Button>
+    </div>
+  );
+};
+
+export default UpdateItemQuantity;

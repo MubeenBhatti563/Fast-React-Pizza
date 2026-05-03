636797d8120ce6717e224143393d51a3d561196d
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-05-03 18:23:30 +0500

    Added UpdateItemQuantity component

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Button from "../../ui/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteItem, getTotalCartQuantityById } from "./cartSlice";
+import UpdateItemQuantity from "./UpdateItemQuantity";
 
 const CartItem = ({ item }) => {
-  const { name, quantity, totalPrice } = item;
+  const { pizzaId, name, quantity, totalPrice } = item;
+  const dispatch = useDispatch();
+  const currentIncrement = useSelector((state) =>
+    getTotalCartQuantityById(state, pizzaId),
+  );
+
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
@@ -10,7 +18,13 @@ const CartItem = ({ item }) => {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">${totalPrice}</p>
-        <Button type="small">Delete</Button>
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentIncrement={currentIncrement}
+        />
+        <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+          Delete
+        </Button>
       </div>
     </li>
   );

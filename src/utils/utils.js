426e19ad6f6f83c12e7cf4bb59431cc7928acc9a
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-05-04 15:16:11 +0500

    Exported getAddress function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,3 +16,12 @@ export const timeFormat = (dateString) => {
 
   return date.toLocaleString("en-US", options).replace(",", "");
 };
+
+export async function getAddress({ latitude, longitude }) {
+  const res = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
+  );
+  if (!res.ok) throw Error("Failed getting address");
+  const data = await res.json();
+  return data;
+}

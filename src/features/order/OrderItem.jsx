ad3f7080a5a9715be140f1860d1646ba7f3801d8
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-05-07 21:28:00 +0500

    Passed props from Order to OrderItem

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-const OrderItem = ({ item }) => {
+const OrderItem = ({ item, isLoadingIngredients, ingredients }) => {
   const { quantity, name, totalPrice } = item;
   return (
-    <li className="py-3">
+    <li className="space-y-1 py-3">
       <div className="flex items-center justify-between gap-4 text-sm px-1">
         <p>
           <span className="font-bold">{quantity}</span> {name}
         </p>
         <p className="font-bold">${totalPrice}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(", ")}
+      </p>
     </li>
   );
 };

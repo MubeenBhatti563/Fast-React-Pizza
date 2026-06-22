9011d53bf6177fbecc8b6ce05f28cc6b4c180627
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2026-06-22 11:39:03 +0500

    Updated settings for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "https://MubeenBhatti563.github.io/Fast-React-Pizza/",
   plugins: [tailwindcss()],
 });
